8df669fdb3762237e99a84fe9c9777cb401050bb
jga01 <46873180+jga01@users.noreply.github.com>
2022-04-06 09:05:08 -0300

    Declaração das variáveis e protótipos de funções

--- main.c
+++ main.c
@@ -8,40 +8,89 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX
+#define MAX 100
 
 // Dados dos servidores
 int codigos_servidores[MAX];
 char nomes_servidores[MAX][255];
 int siape_servidores[MAX];
 int rg_servidores[MAX];
-int cpf_servidores[MAX];
-char endereço_servidores[MAX][255];
+int cpf_serv[MAX];
+char endereco_servidores[MAX][255];
 int salario_servidores[MAX];
 int nascimento_servidores[MAX];
 char tipo_servidores[MAX][255];
 
 //Dados dos carros
 int codigos_veiculos[MAX];
-char descrição_veiculos[MAX][255];
+char descricao_veiculos[MAX][255];
 char placa_veiculos[MAX][255];
 char marca_veiculos[MAX][255];
 char modelo_veiculos[MAX][255];
 
-void criarServidor(int[], char[][], int[], int[], int[], char[][], int[], int[], char[][]);
-void atualizarServidor(int[], char[][], int[], int[], int[], char[][], int[], int[], char[][]);
-void deletarCarro(int[], char[][], int[], int[], int[], char[][], int[], int[], char[][]);
-
-void criarCarro(int[], char[][], char[][], char[][], char[][]);
-void atualizarCarro(int[], char[][], char[][], char[][], char[][]);
+void criarServidor(int[], char[][255], int[], int[], int[], char[][255], int[], int[], char[][255]);
+void atualizarServidor(int[], char[][255], int[], int[], int[], char[][255], int[], int[], char[][255]);
 void deletarCarro(int);
+
+void criarCarro(int[], char[][255], char[][255], char[][255], char[][255]);
+void atualizarCarro(int[], char[][255], char[][255], char[][255], char[][255]);
+void deletarServidor(int);
 
 //Listar tudo
 char listarDados(int);
 
 int main()
 {
-    printf("Ola Mundo!!");
+
+    int input = 2929;
+
+    while(input)
+    {
+        printf("\n");
+        printf("  ___  ___  ___  ___   ___   _    ___  _  __ ___  _  _   ___ \n");
+        printf(" |_ _|| __|/ __|| __| | _ \\ /_\\  | _ \\| |/ /|_ _|| \\| | / __|\n");
+        printf("  | | | _|| (__ | _|  |  _// _ \\ |   /| ' <  | | | .` || (_ |\n");
+        printf(" |___||_|  \\___||___| |_| /_/ \\_\\|_|_\\|_|\\_\\|___||_|\\_| \\___|\n\n");
+
+        printf("1. Registrar servidor.\n");
+        printf("2. Atualizar servidor.\n");
+        printf("3. Deletar servidor.\n");
+        printf("4. Registrar veiculo.\n");
+        printf("5. Atualizar veiculo\n");
+        printf("6. Deletar veiculo.\n");
+        printf("7. Listar\n");
+        printf("0. Sair do programa.\n\n");
+        printf("Input: ");
+
+        scanf("%d", &input);
+
+        switch(input)
+        {
+            case 0:
+                printf("Good bye!");
+                return 0;
+            case 1:
+                printf("Digite o nome do servidor: ");
+                break;
+            case 2:
+                printf("Digite o codigo do servidor: ");
+                break;
+            case 3:
+                printf("Digite o codigo do servidor: ");
+                break;
+            case 4:
+                printf("Digite o nome do carro: ");
+                break;
+            case 5:
+                printf("Digite o codigo do carro: ");
+                break;
+            case 6:
+                printf("Digite o codigo do carro: ");
+                break;
+            case 7:
+                break;
+        }
+    }
 
     return 0;
 }
