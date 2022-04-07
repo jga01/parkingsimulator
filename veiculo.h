1aaba902b5c517da32469bcc49277e45f35aa968
jga01 <46873180+jga01@users.noreply.github.com>
2022-04-07 10:26:31 -0300

    Limpeza da Main

---
+++ veiculo.h
@@ -0,0 +1,18 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
+#define MAX 100
+
+extern int cod_veic[MAX];
+extern int cod_serv[MAX];
+extern char descricao[MAX][255];
+extern char placa[MAX][255];
+extern char marca[MAX][255];
+extern char modelo[MAX][255];
+
+void iniciar();
+void inserir_veiculo(int cod_veic[], int cod_serv[], char descricao[], char placa[], char marca[], char modelo[]);
+void excluir_por_codigo(char cod_veic[]);
+void listar_veiculo();
+
+#endif //VEICULO_H
