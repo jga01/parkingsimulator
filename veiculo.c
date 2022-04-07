03e8387677e7f44aecea7eacdbc5a56301d47cd3
pedro <pedro_freires18@outlook.com>
2022-04-06 21:14:23 -0300

    Inserindo veiculo.c

---
+++ veiculo.c
@@ -0,0 +1,18 @@
+//
+// Created by pedro on 06/04/2022.
+//
+
+/*ÁREA DE PROCESSAMENTO DE DADOS DOS VEICULOS */
+
+void criarCarro(int cod_veic[], char desc_veic[][255], char placa_veic[][255], char marca_veic[][255], char model_veic[][255])
+{
+    return ;
+}
+void atualizarCarro(int cod_veic[], char desc_veic[][255], char placa_veic[][255], char marca_veic[][255], char model_veic[][255])
+{
+    return ;
+}
+void deletarCarro(int cod_veic)
+{
+    return ;
+}
