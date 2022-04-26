17b379cb56cd1a50768ddf9a0dc104c0561d52e5
jga01 <46873180+jga01@users.noreply.github.com>
2022-04-26 12:03:19 -0300

    Reestruturação de veiculos.c e realocação de variáveis e funções nos locais apropriados.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/pedro/CLionProjects/parkingsimulator
+# Install script for directory: C:/Users/tiago/Desktop/C/parkingsimulator
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/pedro/CLionProjects/parkingsimulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/tiago/Desktop/C/parkingsimulator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- servidor.h
+++ servidor.h
@@ -3,6 +3,7 @@
 
 #define MAX 100
 
+extern int ocupados[MAX];
 extern char cod[MAX][255]; // obrigatoria
 extern char nome_serv[MAX][255]; // obrigatoria
 extern char siape_serv[MAX][255];  // obrigatoria
@@ -23,5 +24,9 @@ void ordenando(int opcao);
 int type_serv();
 void printar_serv(int ordenado[]);
 void verificador_quantia(int, int []);
+int checarCodigos(char temp_cod[]);
+int checaEntrada(char codigo[],char nome[], char siape[],char cpf[], char iden[] );
+int existe_cod(char codigo[]);
+void iniciar_ocupados();
 
 #endif //SERVIDOR_H
